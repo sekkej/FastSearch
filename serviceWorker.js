a4da12e2405b5c18aa6844006e9ce18f0b8ab5c7
yer7700 <37212234+yer7700@users.noreply.github.com>
2023-03-27 00:57:34 +0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,5 +2,5 @@ chrome.omnibox.onInputEntered.addListener((text, suggest) => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.remove(tabs[0].id);
   });
-  chrome.tabs.create({ url: "http://ma-kaka.ru/redirector/" + text.replace(" ", "-") });
+  chrome.tabs.create({ url: "http://ma-kaka.ru/api/redirector/" + text.replace(" ", "-") });
 });
